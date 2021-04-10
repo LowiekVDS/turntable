ff77b2be8db1a3fbb415f55bebbb01b778283270
Lowiek Van den Stock <lowiek.van.den.stockt@telenet.be>
2021-04-10 17:55:11 +0200

    fixed stuff

--- src/hardware/hardware.service.ts
+++ src/hardware/hardware.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { SonosService } from "src/sonos/sonos.service";
 import { HardwareControlFactory } from "./hardwarecontrol";
-import { MediumFactory } from "./medium";
 
 @Injectable()
 export class HardwareService {
 
     private hardwareControlInstance = HardwareControlFactory.getInstance();
-    private medium = MediumFactory.getInstance();
 
     constructor() { }
     
